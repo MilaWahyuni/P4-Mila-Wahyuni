8b5924c00a4b4e1ebfc1df96918781aa609fe9d3
MilaWahyuni <64572360+MilaWahyuni@users.noreply.github.com>
2020-05-13 02:47:30 -0700

    Create Jobsheet 2 C++.cpp
    Membuat program mengisi dan menampilkan elemen array dengan index array sesuai dengan Nim terakhir setiap mahasiswa

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,32 @@
+#include <iostream> 
+using namespace std; 
+main ()
+{
+int nim[11];
+cout<<" Nama : Mila Wahyuni\n";
+cout<<" NIM : F1B019091\n";
+cout<<" Kelompok : 15 \n";
+
+cout << "Masukkan nilai ke-1 ";
+cin >> nim[0];
+cout << "Masukkan nilai ke-2 ";
+cin >> nim[1];
+cout << "Masukkan nilai ke-3 ";
+cin >> nim[2];
+cout << "Masukkan nilai ke-4 ";
+cin >> nim[3];
+cout << "Masukkan nilai ke-5 ";
+cin >> nim[4];
+cout << "Masukkan nilai ke-6 ";
+cin >> nim[5];
+cout << "Masukkan nilai ke-7 ";
+cin >> nim[6];
+cout << "Masukkan nilai ke-8 ";
+cin >> nim[7];
+cout << "Masukkan nilai ke-9 ";
+cin >> nim[8];
+cout << "Masukkan nilai ke-10 ";
+cin >> nim[9];
+cout << "Masukkan nilai ke-11 ";
+cin >> nim[10];
+}
