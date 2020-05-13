e0a9b6d9b6f5a8923cf732796f594cbd4c90a375
MilaWahyuni <64572360+MilaWahyuni@users.noreply.github.com>
2020-05-13 02:45:46 -0700

    Create Jobsheet 2 C.cpp
    Membuat program mengisi dan menampilkan elemen array dengan index array sesuai dengan Nim terakhir setiap mahasiswa

---
+++ Jobsheet 2 C.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+main ()
+{
+int nim[11];
+printf(" Nama : Mila Wahyuni\n");
+printf(" NIM : F1B019091\n");
+printf(" Kelompok : 15 \n");
+
+printf ("Masukkan nilai ke-1 ");
+scanf ("%i", &nim[0]);
+printf ("Masukkan nilai ke-2 ");
+scanf ("%i", &nim[1]);
+printf ("Masukkan nilai ke-3 ");
+scanf ("%i", &nim[2]);
+printf ("Masukkan nilai ke-4 ");
+scanf ("%i", &nim[3]);
+printf ("Masukkan nilai ke-5 ");
+scanf ("%i", &nim[4]);
+printf ("Masukkan nilai ke-6 ");
+scanf ("%i", &nim[5]);
+printf ("Masukkan nilai ke-7 ");
+scanf ("%i", &nim[6]);
+printf ("Masukkan nilai ke-8 ");
+scanf ("%i", &nim[7]);
+printf ("Masukkan nilai ke-4 ");
+scanf ("%i", &nim[8]);
+printf ("Masukkan nilai ke-10 ");
+scanf ("%i", &nim[9]);
+printf ("Masukkan nilai ke-11 ");
+scanf ("%i", &nim[10]);
+}
