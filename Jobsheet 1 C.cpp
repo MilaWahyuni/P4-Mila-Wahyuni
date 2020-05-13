f70c7ff1480c0cacfe822987774811031230c4e6
MilaWahyuni <64572360+MilaWahyuni@users.noreply.github.com>
2020-05-13 02:43:21 -0700

    Create Jobsheet 1 C.cpp
    Membuat program array satu dimensi yang menampilkan NIM masing-masing mahasiswa

---
+++ Jobsheet 1 C.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+main ()
+{
+printf("Nama : Mila Wahyuni\n"); printf("NIM : F1B019091\n"); printf("Kelompok : 15\n");
+
+
+int nim[11]=
+{65,3,20,49,37,44,24,54,74,67,73};
+
+printf ("Nim ke-1 = %i\n",nim[0]); 
+printf ("Nim ke-2 = %i\n",nim[1]); 
+printf ("Nim ke-3 = %i\n",nim[2]); 
+printf ("Nim ke-4 = %i\n",nim[3]); 
+printf ("Nim ke-5 = %i\n",nim[4]); 
+printf ("Nim ke-6 = %i\n",nim[5]); 
+printf ("Nim ke-7 = %i\n",nim[6]); 
+printf ("Nim ke-8 = %i\n",nim[7]); 
+printf ("Nim ke-9 = %i\n",nim[8]); 
+printf ("Nim ke-10 = %i\n",nim[9]); 
+printf ("Nim ke-11= %i\n",nim[10]);
+
+}
